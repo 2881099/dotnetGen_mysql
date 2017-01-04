22ce95904fb361a4d2dbc228b39e9a55af814d4f
2881099 <2881099@qq.com>
2017-01-04 16:56:31 +0800

    WhereXxxLike 优化，未传值则不构成条件

--- Server/CodeBuild(Code).cs
+++ Server/CodeBuild(Code).cs
@@ -1627,7 +1627,7 @@ namespace {0}.BLL {{
 		}}", uClass_Name, fkcsBy, csType, col.Name);
 						sb6.AppendFormat(@"
 		public {0}SelectBuild Where{1}Like(params {2}[] {1}) {{
-			if ({1} == null) return this;
+			if ({1} == null || {1}.Where(a => !string.IsNullOrEmpty(a)).Any() == false) return this;
 			return this.Where1Or(@""a.`{3}` LIKE {{0}}"", {1}.Select(a => ""%"" + a + ""%"").ToArray());
 		}}", uClass_Name, fkcsBy, csType, col.Name);
 						return;
