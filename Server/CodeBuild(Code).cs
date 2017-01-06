e8f4bc00873b85034222abdb300ca1ddbad8d42c
2881099 <2881099@qq.com>
2017-01-06 11:48:49 +0800

    WhereId 参数非可空

--- Server/CodeBuild(Code).cs
+++ Server/CodeBuild(Code).cs
@@ -1496,7 +1496,7 @@ namespace {0}.BLL {{
 						sb6.AppendFormat(@"
 		public {0}SelectBuild Where{1}(params {2}[] {1}) {{
 			return this.Where1Or(""a.`{3}` = {{0}}"", {1});
-		}}", uClass_Name, fkcsBy, csType, col.Name);
+		}}", uClass_Name, fkcsBy, col.IsPrimaryKey ? csType.Replace("?", "") : csType, col.Name);
 						return;
 					}
 					if (col.Type == MySqlDbType.Double || col.Type == MySqlDbType.Float || col.Type == MySqlDbType.Decimal) {
