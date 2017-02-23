ed7a68c80d6ceef9df509aca4e34bd4be0b7f670
2881099 <2881099@qq.com>
2017-02-23 17:59:35 +0800

    top.mainViewNav.reload

--- Server/CodeBuild(Const).cs
+++ Server/CodeBuild(Const).cs
@@ -1943,6 +1943,7 @@ namespace {0}.AdminControllers {{
 									}});
 								}}
 							}},
+							reload: startRouter,
 							query: qs_parseByUrl(refererUrl)
 						}};
 						top.mainViewInit = function () {{
