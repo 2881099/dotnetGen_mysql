b354323419d36997b903f595ad3499a0581b5bb5
叶湘勤 <2881099@qq.com>
2018-02-28 16:01:13 +0800

    RedisHelper.NewMongodbId()算法优化

--- Server/Resources/CSRedis/QuickHelperBase.cs
+++ Server/Resources/CSRedis/QuickHelperBase.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Threading;
 
 namespace CSRedis {
 	public partial class QuickHelperBase {
@@ -9,20 +11,32 @@ namespace CSRedis {
 
 		private static DateTime dt1970 = new DateTime(1970, 1, 1);
 		private static Random rnd = new Random();
+		private static readonly int __staticMachine = ((0x00ffffff & Environment.MachineName.GetHashCode()) +
+#if NETSTANDARD1_5 || NETSTANDARD1_6
+			1
+#else
+            AppDomain.CurrentDomain.Id
+#endif
+			) & 0x00ffffff;
+		private static readonly int __staticPid = Process.GetCurrentProcess().Id;
+		private static int __staticIncrement = rnd.Next();
 		/// <summary>
-		/// 生成类似Mongodb的ObjectId有序Guid
+		/// 生成类似Mongodb的ObjectId有序、不重复Guid
 		/// </summary>
 		/// <returns></returns>
 		public static Guid NewMongodbId() {
 			var now = DateTime.Now;
-			var uninxtime = (int)DateTime.Now.Subtract(dt1970).TotalSeconds;
-			var key = $"NewMongodbId{now.ToString("yyyyMMddHH")}";
-			var value = (long)Increment(key);
-			Expire(key, TimeSpan.FromHours(24));
+			var uninxtime = (int)now.Subtract(dt1970).TotalSeconds;
+			int increment = Interlocked.Increment(ref __staticIncrement) & 0x00ffffff;
 			var rand = rnd.Next(0, int.MaxValue);
-			//e8f35037-887d-4f64-8355-f96e02e71807
-			var guid = $"{uninxtime.ToString("x8").PadLeft(8, '0')}{rand.ToString("x8").PadLeft(8, '0')}{value.ToString("x8").PadLeft(16, '0')}";
+			var guid = $"{uninxtime.ToString("x8").PadLeft(8, '0')}{__staticMachine.ToString("x8").PadLeft(8, '0').Substring(2, 6)}{__staticPid.ToString("x8").PadLeft(8, '0').Substring(6, 2)}{increment.ToString("x8").PadLeft(8, '0')}{rand.ToString("x8").PadLeft(8, '0')}";
 			return Guid.Parse(guid);
+			//var value = HashIncrement("NewMongodbIdyyyyMMdd", now.ToString("HH:mm"), 1);
+			//if (value == 1) Expire("NewMongodbIdyyyyMMdd", TimeSpan.FromHours(24));
+			//var rand = rnd.Next(0, int.MaxValue);
+			////e8f35037-887d-4f64-8355-f96e02e71807
+			//var guid = $"{uninxtime.ToString("x8").PadLeft(8, '0')}{rand.ToString("x8").PadLeft(8, '0')}{value.ToString("x8").PadLeft(16, '0')}";
+			//return Guid.Parse(guid);
 		}
 		/// <summary>
 		/// 设置指定 key 的值
