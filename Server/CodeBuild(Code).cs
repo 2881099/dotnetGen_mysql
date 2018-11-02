b45a635a32b4848760ace85feb7fb1d70938100f
28810 <28810@YEXIANGQIN>
2018-11-02 12:56:57 +0800

    增加非主键的外键支持

--- Server/CodeBuild(Code).cs
+++ Server/CodeBuild(Code).cs
@@ -561,21 +561,22 @@ namespace {0}.Model {{
 					int ms = 0;
 					//若中间表，两外键指向相同表，则选择 表名_主键名 此字段作为主参考字段
 					string main_column = fk.Columns[0].Name;
+					var pkName = fk.ReferencedColumns[0].Name;
 					foreach (ColumnInfo columnInfo in t2.Columns) {
 						string csType = GetCSType(columnInfo.Type, "", columnInfo.SqlType);
 						bool is_addignore = columnInfo.IsPrimaryKey && csType == "Guid?" ||
 							 columnInfo.Name.ToLower() == "update_time" && csType == "DateTime?" ||
 							 columnInfo.Name.ToLower() == "create_time" && csType == "DateTime?";
 						if (string.Compare(columnInfo.Name, main_column, true) == 0) {
-							parmsNoneType2 += string.Format("\r\n			{0} = this.{1}, ", CodeBuild.UFString(columnInfo.Name), CodeBuild.UFString(table.PrimaryKeys[0].Name));
-							//if (!is_addignore) parmsNoneType2_add += string.Format("\r\n			{0} = this.{1}, ", CodeBuild.UFString(columnInfo.Name), CodeBuild.UFString(table.PrimaryKeys[0].Name));
+							parmsNoneType2 += string.Format("\r\n			{0} = this.{1}, ", CodeBuild.UFString(columnInfo.Name), CodeBuild.UFString(pkName));
+							//if (!is_addignore) parmsNoneType2_add += string.Format("\r\n			{0} = this.{1}, ", CodeBuild.UFString(columnInfo.Name), CodeBuild.UFString(pkName));
 
-							parmsNoneType4 += string.Format(GetCSTypeValue(columnInfo.Type), "this." + CodeBuild.UFString(table.PrimaryKeys[0].Name)) + ", ";
-							parmsNoneType5 += string.Format("\r\n			item.{0} = this.{1};", CodeBuild.UFString(columnInfo.Name), CodeBuild.UFString(table.PrimaryKeys[0].Name));
-							if (columnInfo.IsPrimaryKey) pkNamesNoneType += string.Format(GetCSTypeValue(table.PrimaryKeys[0].Type), "this." + CodeBuild.UFString(table.PrimaryKeys[0].Name)) + ", ";
+							parmsNoneType4 += string.Format(GetCSTypeValue(columnInfo.Type), "this." + CodeBuild.UFString(pkName)) + ", ";
+							parmsNoneType5 += string.Format("\r\n			item.{0} = this.{1};", CodeBuild.UFString(columnInfo.Name), CodeBuild.UFString(pkName));
+							if (columnInfo.IsPrimaryKey) pkNamesNoneType += string.Format(GetCSTypeValue(fk.ReferencedColumns[0].Type), "this." + CodeBuild.UFString(pkName)) + ", ";
 							continue;
 						}
-						if (columnInfo.IsPrimaryKey) pkNamesNoneType += string.Format(GetCSTypeValue(table.PrimaryKeys[0].Type), CodeBuild.UFString(columnInfo.Name)) + ", ";
+						if (columnInfo.IsPrimaryKey) pkNamesNoneType += string.Format(GetCSTypeValue(columnInfo.Type), CodeBuild.UFString(columnInfo.Name)) + ", ";
 						else if (columnInfo.Name.ToLower() == "create_time" && csType == "DateTime?") ;
 						else updateDiySet += string.Format("\r\n\t\t\t\t.Set{0}({0})", CodeBuild.UFString(columnInfo.Name));
 
@@ -709,18 +710,18 @@ namespace {0}.Model {{
 		public int Unflag{1}({4}) => BLL.{0}.Delete{9}({5});
 		public int Unflag{1}ALL() => BLL.{0}.DeleteBy{8}(this.{7});
 ", CodeBuild.UFString(t2.ClassName), CodeBuild.UFString(addname_schema), parms3, parmsNoneType3, parms4, parmsNoneType4,
-	solutionName, CodeBuild.UFString(table.PrimaryKeys[0].Name), CodeBuild.UFString(fk.Columns[0].Name), deleteByUniqui);
+	solutionName, CodeBuild.UFString(pkName), CodeBuild.UFString(fk.Columns[0].Name), deleteByUniqui);
 						sb17.AppendFormat(@"
 		async public Task<int> Unflag{1}Async({2}) => await Unflag{1}Async({3});
 		async public Task<int> Unflag{1}Async({4}) => await BLL.{0}.Delete{9}Async({5});
 		async public Task<int> Unflag{1}ALLAsync() => await BLL.{0}.DeleteBy{8}Async(this.{7});
 ", CodeBuild.UFString(t2.ClassName), CodeBuild.UFString(addname_schema), parms3, parmsNoneType3, parms4, parmsNoneType4,
-	solutionName, CodeBuild.UFString(table.PrimaryKeys[0].Name), CodeBuild.UFString(fk.Columns[0].Name), deleteByUniqui);
+	solutionName, CodeBuild.UFString(pkName), CodeBuild.UFString(fk.Columns[0].Name), deleteByUniqui);
 
 						if (ms > 2) {
 
 						} else {
-							string civ = string.Format(GetCSTypeValue(table.PrimaryKeys[0].Type), "_" + CodeBuild.UFString(table.PrimaryKeys[0].Name));
+							string civ = string.Format(GetCSTypeValue(fk.ReferencedColumns[0].Type), "_" + CodeBuild.UFString(pkName));
 							string f5 = t2name;
 							//if (addname != f5) {
 							string fk20_ReferencedTable_Name = fk2[0].ReferencedTable.Name;
@@ -774,7 +775,7 @@ namespace {0}.Model {{
 		public {0}Info Obj_{1} {{
 			get {{ return _obj_{1} ?? (_{4} == null ? null : (_obj_{1} = BLL.{0}.GetItem(_{5}))); }}
 			internal set {{ _obj_{1} = value; }}
-		}}", CodeBuild.UFString(t2.ClassName), CodeBuild.LFString(t2.ClassName), solutionName, CodeBuild.UFString(fk.Columns[0].Name), CodeBuild.UFString(table.PrimaryKeys[0].Name), string.Format(GetCSTypeValue(table.PrimaryKeys[0].Type), UFString(table.PrimaryKeys[0].Name)));
+		}}", CodeBuild.UFString(t2.ClassName), CodeBuild.LFString(t2.ClassName), solutionName, CodeBuild.UFString(fk.Columns[0].Name), CodeBuild.UFString(fk.ReferencedColumns[0].Name), string.Format(GetCSTypeValue(fk.ReferencedColumns[0].Type), UFString(fk.ReferencedColumns[0].Name)));
 							string objs_key = string.Format("Obj_{0}s", f2);
 							if (!dic_objs.ContainsKey(objs_key))
 								dic_objs.Add(objs_key, obj_value);
