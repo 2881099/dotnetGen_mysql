eddf97e04cc0a65e08012a6020a8e4a3153d24f5
28810 <28810@YEXIANGQIN>
2018-11-26 12:27:04 +0800

    .Update 方法增加 ignoreFields 参数，过滤不想被更新的字段

--- Server/CodeBuild(Code).cs
+++ Server/CodeBuild(Code).cs
@@ -1236,7 +1236,7 @@ namespace {0}.DAL {{
 							}
 						}
 						sb6.AppendFormat(@"
-				.Set{0}(item.{0})", CodeBuild.UFString(col.Name));
+			if (ignore.ContainsKey(""{1}"") == false) sub.Set{0}(item.{0});", CodeBuild.UFString(col.Name), col.Name);
 					}
 
 					string dal_insert_code = string.Format(@"
@@ -1306,8 +1306,10 @@ namespace {0}.DAL {{
 					sb1.AppendFormat(@"
 {1}
 
-		public SqlUpdateBuild Update({0}Info item) {{
-			return new SqlUpdateBuild(new List<{0}Info> {{ item }}){8};
+		public SqlUpdateBuild Update({0}Info item, string[] ignoreFields) {{
+			var sub = new SqlUpdateBuild(new List<{0}Info> {{ item }});
+			var ignore = ignoreFields?.ToDictionary(a => a, StringComparer.CurrentCultureIgnoreCase) ?? new Dictionary<string, string>();{8}
+			return sub;
 		}}
 		#region class SqlUpdateBuild
 		public partial class SqlUpdateBuild {{
@@ -1507,7 +1509,7 @@ namespace {0}.BLL {{
 
 					if (uniques_dic.Count > 1)
 						sb1.AppendFormat(@"
-		public static int Update({1}Info item) => dal.Update(item).ExecuteNonQuery();
+		public static int Update({1}Info item, params string[] ignoreFields) => dal.Update(item, ignoreFields).ExecuteNonQuery();
 		public static {0}.DAL.{1}.SqlUpdateBuild UpdateDiy({2}) => new {0}.DAL.{1}.SqlUpdateBuild(new List<{1}Info> {{ itemCacheTimeout > 0 ? new {1}Info {{ {4} }} : GetItem({3}) }});
 		public static {0}.DAL.{1}.SqlUpdateBuild UpdateDiy(List<{1}Info> dataSource) => new {0}.DAL.{1}.SqlUpdateBuild(dataSource);
 		/// <summary>
@@ -1521,7 +1523,7 @@ namespace {0}.BLL {{
 							xxxxtempskdf += xxxxtempskdfstr + " = " + xxxxtempskdfstr + ", ";
 						}
 						sb1.AppendFormat(@"
-		public static int Update({1}Info item) => dal.Update(item).ExecuteNonQuery();
+		public static int Update({1}Info item, params string[] ignoreFields) => dal.Update(item, ignoreFields).ExecuteNonQuery();
 		public static {0}.DAL.{1}.SqlUpdateBuild UpdateDiy({2}) => new {0}.DAL.{1}.SqlUpdateBuild(new List<{1}Info> {{ new {1}Info {{ {4} }} }});
 		public static {0}.DAL.{1}.SqlUpdateBuild UpdateDiy(List<{1}Info> dataSource) => new {0}.DAL.{1}.SqlUpdateBuild(dataSource);
 		/// <summary>
@@ -1532,7 +1534,7 @@ namespace {0}.BLL {{
 					}
 
 					bll_async_code += string.Format(@"
-		async public static Task<int> UpdateAsync({1}Info item) => await dal.Update(item).ExecuteNonQueryAsync();
+		async public static Task<int> UpdateAsync({1}Info item, params string[] ignoreFields) => await dal.Update(item, ignoreFields).ExecuteNonQueryAsync();
 ", solutionName, uClass_Name);
 
 					if (table.Columns.Count > 5)
